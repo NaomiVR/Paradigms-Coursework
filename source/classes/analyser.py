a601820a992302004e9b00b6f45dc9691da1e9ea
NaomiVR <123805883+NaomiVR@users.noreply.github.com>
2023-01-28 12:24:13 +0000

    Update analyser.py

--- source/classes/analyser.py
+++ source/classes/analyser.py
@@ -6,6 +6,13 @@ class Analyser:
         self.data = data
 
     def calc_friend_total(self, user):
+        """
+        It takes a user as an argument, checks if the user exists in the data dictionary, and if so, returns the number
+        of friends the user has
+
+        :param user: The user whose friends we want to count
+        :return: The number of friends a user has.
+        """
         friends = self.data.get(user)
         if friends is not None:
             total_friends = len(friends)
@@ -15,6 +22,20 @@ class Analyser:
             return f'{Fore.RED} User not found{Style.RESET_ALL}'
 
     def least_friends(self):
+        """
+        We loop through the data, keeping track of the number of friends each user has, and the user with the least
+        friends.
+
+        If we find a user with fewer friends than the current user with the least friends, we update the user with the
+        least friends.
+
+        If we find a user with the same number of friends as the current user with the least friends, we add that user
+        to the list of users with the least friends.
+
+        Finally, we return a string with the users with the least friends.
+
+        :return: A list of users with the least number of friends.
+        """
         least_friends = float('inf')
         users_with_least_friends = []
         for user, friends in self.data.items():
@@ -32,6 +53,12 @@ class Analyser:
         return results
 
     def direct_relationships(self, user):
+        """
+        If the user exists, return a list of their friends, otherwise return an error message
+
+        :param user: The user whose direct relationships we want to see
+        :return: A string with the direct relationships of the user.
+        """
         friends = self.data.get(user)
         if friends is not None:
             if friends:
@@ -42,6 +69,12 @@ class Analyser:
             return f'{Fore.RED}User not found{Style.RESET_ALL}'
 
     def indirect_relationships(self):
+        """
+        For each user, get the list of their friends, and then for each of those friends, get the list of their friends,
+        and then remove the user's friends and the user from the list
+
+        :return: A dictionary of users and their indirect friends.
+        """
         indirect_friends = {}
         for user, friends in self.data.items():
             if not friends:
