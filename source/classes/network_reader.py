c71c42ae9c80cdbf7cf145577c68b71b57ce6686
NaomiVR <123805883+NaomiVR@users.noreply.github.com>
2023-01-28 12:25:38 +0000

    Update network_reader.py

--- source/classes/network_reader.py
+++ source/classes/network_reader.py
@@ -9,6 +9,10 @@ class NetworkReader:
         self._social_nw = {}
 
     def get_nw_data(self):
+        """
+        It reads the file line by line, and for each line, it splits the line into parts, and then adds the parts to the
+        social network
+        """
         with open(self._filename, 'r') as f:
             self._num_of_users = f.readline().strip()
             print(f'Total users: {self._num_of_users}')
@@ -32,6 +36,9 @@ class NetworkReader:
                     self._social_nw[user] = []
 
     def validate_nw_data(self):
+        """
+        It checks if the friend of a friend is also a friend of the original person
+        """
         for key, value in self._social_nw.items():
             for friend in value:
                 if key not in self._social_nw[friend]:
@@ -42,6 +49,11 @@ class NetworkReader:
                     print('Network is consistent.')
 
     def display_nw_data(self):
+        """
+        It takes the dictionary of the social network and returns a string of the social network in the format of:
+        
+        :return: The result is a string that contains the social network data.
+        """
         result = ''
         for key, value in self._social_nw.items():
             value_str = ', '.join(value)
