d5094c617305b7841a9330205ad7659f17524199
NaomiVR <123805883+NaomiVR@users.noreply.github.com>
2023-01-29 11:53:32 +0000

    Update __init__.py

--- source/testing/integration/__init__.py
+++ source/testing/integration/__init__.py
@@ -1,4 +1,5 @@
 from colorama import Fore, Style
+from source.classes.custom_exceptions import FileConsistencyError
 
 
 class NetworkReader:
@@ -8,29 +9,46 @@ class NetworkReader:
         self._social_nw = {}
 
     def get_nw_data(self):
-        with open(self._filename, 'r') as f:
-            self._num_of_users = f.readline().strip()
-            print(f'Total users: {self._num_of_users}')
+        """
+        It reads the file line by line, and for each line, it splits the line into parts, and then adds the parts to the
+        social network
+        """
+        # Reading the first line of the file and assigning it to the variable `self._num_of_users`
+        try:
+            with open(self._filename, 'r') as f:
+                self._num_of_users = f.readline().strip()
+                print(f'Total users: {self._num_of_users}')
 
-            users = []
+                users = []
 
-            for line in f:
-                parts = line.strip().split()
+                # Reading the file line by line, and for each line, it splits the line into parts, and then adds the
+                # parts to the social network.
+                for line in f:
+                    parts = line.strip().split()
 
-                if len(parts) == 2:
-                    if parts[0] not in self._social_nw:
-                        self._social_nw[parts[0]] = []
-                    if parts[1] not in self._social_nw:
-                        self._social_nw[parts[1]] = []
-                    self._social_nw[parts[0]].append(parts[1])
-                    self._social_nw[parts[1]].append(parts[0])
-                users.extend(parts)
+                    if len(parts) == 2:
+                        if parts[0] not in self._social_nw:
+                            self._social_nw[parts[0]] = []
+                        if parts[1] not in self._social_nw:
+                            self._social_nw[parts[1]] = []
+                        self._social_nw[parts[0]].append(parts[1])
+                        self._social_nw[parts[1]].append(parts[0])
+                    # Adding the parts to the list `users`.
+                    users.extend(parts)
 
-            for user in set(users):
-                if user not in self._social_nw:
-                    self._social_nw[user] = []
+                # Checking if there is any user in the list `users` that is not in the social network. If there is, then
+                # it adds that user to the social network.
+                for user in set(users):
+                    if user not in self._social_nw:
+                        self._social_nw[user] = []
+        except OSError as e:
+            print(f'Failed with exception {e}, pleasse try again')
+            return False
 
     def validate_nw_data(self):
+        """
+        It checks if the friend of a friend is also a friend of the original person, if not then it raises an exception
+        """
         for key, value in self._social_nw.items():
             for friend in value:
                 if key not in self._social_nw[friend]:
@@ -41,6 +59,11 @@ class NetworkReader:
                     print('Network is consistent.')
 
     def display_nw_data(self):
+        """
+        It takes the dictionary of the social network and returns a string of the social network in the format of:
+
+        :return: The result is a string that contains the social network data.
+        """
         result = ''
         for key, value in self._social_nw.items():
             value_str = ', '.join(value)
